3ddf4ee6783c28a70c71673cd879baef0e6faff5
03sunpeng <137990943+03sunpeng@users.noreply.github.com>
2023-07-04 14:10:32 +0800

    Create Littleclass.java

---
+++ Kindergarden/bin/entity/exclass/Littleclass.java
@@ -0,0 +1,13 @@
+package entity.exclass;
+
+import entity.Baseclass;
+
+public class Littleclass extends Baseclass {
+	public Littleclass() {
+		super();
+	}
+	public Littleclass(int id) {
+		super(id);
+		setCost(2000);
+	}
+}
