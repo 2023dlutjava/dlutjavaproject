3e6ed3e0c2421dde37fd6627bf3c1bd07f4630b8
03sunpeng <137990943+03sunpeng@users.noreply.github.com>
2023-07-04 14:11:46 +0800

    Create Midclass.java

---
+++ Kindergarden/bin/entity/exclass/Midclass.java
@@ -0,0 +1,13 @@
+package entity.exclass;
+
+import entity.Baseclass;
+
+public class Midclass extends Baseclass {
+	public Midclass() {
+		super();
+	}
+	public Midclass(int id) {
+		super(id);
+		setCost(2500);
+	}
+}
