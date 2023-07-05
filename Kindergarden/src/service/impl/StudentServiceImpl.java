3d20250e010abc6a827e6bcaa8635be145da5856
abandcd <108215662+abandcd@users.noreply.github.com>
2023-07-05 22:06:22 +0800

    Add files via upload

--- Kindergarden/src/service/impl/StudentServiceImpl.java
+++ Kindergarden/src/service/impl/StudentServiceImpl.java
@@ -19,6 +19,31 @@ import java.util.Scanner;
 
 
 public class StudentServiceImpl implements ChangeStudent, UpdateStudent, ViaStuToLesson, ViewStudent {
+	/*
+	 * @author 王家豪 
+	 * 打印学生表
+	 */
+	public void printList(List<Student> lis) {
+		System.out.println("学生序号\t学生姓名\t学生班级");
+		for(int i=0;i<lis.size();i++) {
+			Student stu=lis.get(i);
+			System.out.print(stu.getId()+"\t"+stu.getName()+"\t");
+			int viscla=stu.getClas();
+			if(viscla/10==1)System.out.println("小"+viscla/10+"班");
+			if(viscla/10==2)System.out.println("中"+viscla/10+"班\t");
+			if(viscla/10==3)System.out.println("大"+viscla/10+"班\t");
+			
+		}
+	}
+	/*
+	 * 打印课程表
+	 */
+	public void printLessonList(List<lesson> les) {
+		for(int i=0;i<les.size();i++) {
+			lesson less=les.get(i);
+			System.out.println("课程序号："+less.getId()+"\t\t课程名字"+less.getName());
+		}
+	}
 	 /*
 	  * 根据学生ID查看学生信息
 	  */
@@ -27,12 +52,10 @@ public class StudentServiceImpl implements ChangeStudent, UpdateStudent, ViaStuT
 		StudentDAO studentDAO = new StudentDAOImpl();;//注入StudentDAO对象
 		String sql="select * from student where id=?" ;
 		List<Student> students=new ArrayList<Student>();
-		Object[] str=new String[4];
-		str[0]="\""+id+"\"";
+		Object[] str= {id};
 		students=studentDAO.selectStudent(sql, str);
 		System.out.print("学生信息为：");
-		System.out.println(students);
-		
+		 printList(students);
 	}
 	/*
 	 * 根据学生ID找到学生所选课程
@@ -42,11 +65,10 @@ public class StudentServiceImpl implements ChangeStudent, UpdateStudent, ViaStuT
 		lessonDAO   llessonDAO=new lessonDAOImpl();//注入lessonDAO对象
 		String sql="select * from study where stuid=?";
 		List<lesson> lessons=new ArrayList<lesson>();
-		Object[] str=new String[4];
-		str[0]="\""+id+"\"";
+		Object[] str= {id};
 		lessons=llessonDAO.selectlesson(sql, str);
 		System.out.print("该学生所选课程为:");
-		System.out.println(lessons);
+		printLessonList(lessons);
 	}
 	/*
 	 * 更新学生表中的学生信息
@@ -56,8 +78,7 @@ public class StudentServiceImpl implements ChangeStudent, UpdateStudent, ViaStuT
 		StudentDAO studentDAO = new StudentDAOImpl();;//注入StudentDAO对象
 		if(type==1) {
 		String sql="delete from student where id=?";
-		Object[] str=new String[4];
-		str[0]="\""+id+"\"";
+		Object[] str= {id};
 		int flag=studentDAO.updateStu(sql, str);
 		if(flag>0) {
 		System.out.print("删除成功！！");
@@ -74,10 +95,7 @@ public class StudentServiceImpl implements ChangeStudent, UpdateStudent, ViaStuT
 			int clas=input.nextInt();
 			
 			String sql="insert into student (id,name,class) values(?,?,?)";
-			Object[] str=new String[4];
-			str[0]="\""+id+"\"";
-			str[1]="\""+name+"\"";
-			str[0]="\""+clas+"\"";
+			Object[] str= {id,name,clas};
 			
 			int flag=studentDAO.updateStu(sql, str);
 			if(flag>0) {
@@ -108,9 +126,7 @@ public class StudentServiceImpl implements ChangeStudent, UpdateStudent, ViaStuT
 			System.out.print("请输入更改后的学生id：");
 			int id1=input1.nextInt();
 			String sql="update student set id='?' where id=?";
-			Object[] str=new String[4];
-			str[0]="\""+id1+"\"";
-			str[1]="\""+id+"\"";
+			Object[] str= {id1,id};
 			int flag=studentDAO.updateStu(sql, str);
 			if(flag>0) {
 			System.out.print("更改成功！！");
@@ -124,9 +140,7 @@ public class StudentServiceImpl implements ChangeStudent, UpdateStudent, ViaStuT
 			System.out.print("请输入更改后的学生name：");
 			String name=input1.next();
 			String sql2="update student set name='?' where id=?";
-			Object[] str2=new String[4];
-			str2[0]="\""+name+"\"";
-			str2[1]="\""+id+"\"";
+			Object[] str2= {name,id};
 			int flag2=studentDAO.updateStu(sql2, str2);
 			if(flag2>0) {
 			System.out.print("更改成功！！");
@@ -140,9 +154,7 @@ public class StudentServiceImpl implements ChangeStudent, UpdateStudent, ViaStuT
 			System.out.print("请输入更改后的学生class：");
 			String classs=input1.next();
 			String sql3="update student set class='?' where id=?";
-			Object[] str3=new String[4];
-			str3[0]="\""+classs+"\"";
-			str3[1]="\""+id+"\"";
+			Object[] str3= {classs,id};
 			int flag3=studentDAO.updateStu(sql3, str3);
 			if(flag3>0) {
 			System.out.print("更改成功！！");
